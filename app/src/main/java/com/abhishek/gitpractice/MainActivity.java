1258f6703bc06c6041402a2afbf6dfb45c0aa458
abhis <shindeabhishek100@gmail.com>
2021-10-25 22:51:01 +0530

    build varients and product flavours

--- app/src/main/java/com/abhishek/gitpractice/MainActivity.java
+++ app/src/main/java/com/abhishek/gitpractice/MainActivity.java
@@ -2,13 +2,25 @@ package com.abhishek.gitpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList arrayList=new ArrayList();
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent it=new Intent(MainActivity.this, ExoPlayerActivity.class);
+        it.putParcelableArrayListExtra("nana", arrayList);
+        startActivity(it);
+
     }
 }
